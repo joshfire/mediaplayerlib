be13445690b3e1a659dbbd83fc5dac8438387863
stephan83 <stephan.florquin@gmail.com>
2012-04-13 16:02:15 +0200

    Added HTML5 audio builder.

--- test/lib/mediaplayerlib_test.js
+++ test/lib/mediaplayerlib_test.js
@@ -22,6 +22,11 @@ var mp4 = {
   "contentURL": "http://medias2.francetv.fr/videosread/francetv/m//hbbtv-philips/2012/S13/J3/61232926-20120328-1400k.mp4"
 };
 
+var mp3 = {
+  "@type": "AudioObject",
+  "contentURL": "http://www.largesound.com/ashborytour/sound/brobob.mp3"
+};
+
 var script = {
   "@type": "VideoObject",
   "itemType": "VideoObject",
@@ -29,7 +34,7 @@ var script = {
   "embedURL": "http://google.com"
 };
 
-var videoHelpers = {
+var helpers = {
   youtube: function(options) {
     return function() {
       mediaFactory.resolve(youtube, options).toHtml(this.callback);
@@ -43,6 +48,11 @@ var videoHelpers = {
   mp4: function(options) {
     return function() {
       mediaFactory.resolve(mp4, options).toHtml(this.callback);
+    }
+  },
+  mp3: function(options) {
+    return function() {
+      mediaFactory.resolve(mp3, options).toHtml(this.callback);
     }
   },
   scriptTest: function(options) {
@@ -61,8 +71,11 @@ var videoHelpers = {
   iframe: function(err, html) {
     assert.isNotNull(html.match(/<iframe[^>]*>/));
   },
-  html5: function(err, html) {
+  html5Video: function(err, html) {
     assert.isNotNull(html.match(/<video[^>]*>/));
+  },
+  html5Audio: function(err, html) {
+    assert.isNotNull(html.match(/<audio[^>]*>/));
   },
   script: function(err, html) {
     assert.isNotNull(html.match(/<script[^>]*>/));
@@ -73,51 +86,57 @@ var videoHelpers = {
   noFlashFallback: function(err, html) {
     assert.isNull(html.match(/video-js/));
   }
-}
+};
 
 vows.describe('Media Player Lib').addBatch({
   'Using the default strategy on a Youtube video': {
-    topic: videoHelpers.youtube(),
-    'should not return an error': videoHelpers.noError,
-    'should return an iframe': videoHelpers.iframe
+    topic: helpers.youtube(),
+    'should not return an error': helpers.noError,
+    'should return an iframe': helpers.iframe
   },
   'Using the default strategy on a Vimeo video': {
-    topic: videoHelpers.vimeo(),
-    'should not return an error': videoHelpers.noError,
-    'should return an iframe': videoHelpers.iframe
+    topic: helpers.vimeo(),
+    'should not return an error': helpers.noError,
+    'should return an iframe': helpers.iframe
   },
   'Using the default strategy on an mp4 video': {
-    topic: videoHelpers.mp4(),
-    'should not return an error': videoHelpers.noError,
-    'should return an html5 video': videoHelpers.html5,
-    'should have a Flash fallback': videoHelpers.flashFallback
+    topic: helpers.mp4(),
+    'should not return an error': helpers.noError,
+    'should return an html5 video': helpers.html5Video,
+    'should have a Flash fallback': helpers.flashFallback
   },
   'Using the default strategy on an script embed': {
-    topic: videoHelpers.scriptTest(),
-    'should not return an error': videoHelpers.noError,
-    'should return a script': videoHelpers.script
+    topic: helpers.scriptTest(),
+    'should not return an error': helpers.noError,
+    'should return a script': helpers.script
   },
   //--------------------------------------------------------------------------
   'Using the html5 strategy on a Youtube video': {
-    topic: videoHelpers.youtube({strategy: 'html5'}),
-    'should not return an error': videoHelpers.noError,
-    'should return an iframe': videoHelpers.iframe
+    topic: helpers.youtube({strategy: 'html5'}),
+    'should not return an error': helpers.noError,
+    'should return an iframe': helpers.iframe
   },
   'Using the html5 strategy on a Vimeo video': {
-    topic: videoHelpers.vimeo({strategy: 'html5'}),
-    'should not return an error': videoHelpers.noError,
-    'should return an iframe': videoHelpers.iframe
+    topic: helpers.vimeo({strategy: 'html5'}),
+    'should not return an error': helpers.noError,
+    'should return an iframe': helpers.iframe
   },
   'Using the html5 strategy on an mp4 video': {
-    topic: videoHelpers.mp4({strategy: 'html5'}),
-    'should not return an error': videoHelpers.noError,
-    'should return an html5 video': videoHelpers.html5,
-    'should not have a Flash fallback': videoHelpers.noFlashFallback
+    topic: helpers.mp4({strategy: 'html5'}),
+    'should not return an error': helpers.noError,
+    'should return an html5 video': helpers.html5Video,
+    'should not have a Flash fallback': helpers.noFlashFallback
+  },
+  'Using the html5 strategy on an mp3 audio file': {
+    topic: helpers.mp3({strategy: 'html5'}),
+    'should not return an error': helpers.noError,
+    'should return an html5 audio tag': helpers.html5Audio,
+    'should not have a Flash fallback': helpers.noFlashFallback
   },
   //--------------------------------------------------------------------------
   'Using the oembed strategy on a Vimeo video': {
-    topic: videoHelpers.vimeo({strategy: 'oembed'}),
-    'should not return an error': videoHelpers.noError,
+    topic: helpers.vimeo({strategy: 'oembed'}),
+    'should not return an error': helpers.noError,
     'should not return html': function(err, html) {
       assert.isTrue(typeof 'html' === 'string' && html.length > 0);
     }
